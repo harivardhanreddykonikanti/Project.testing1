db570be7a83448f91e21ddbe30b28c559eb9a37c
harivardhanreddykonikanti <harivardhanreddyking@gmail.com>
2025-03-10 16:21:55 +0530

    modified

--- Testing_For_GitHub/src/TestingPackage_1/NewClassTesting.java
+++ Testing_For_GitHub/src/TestingPackage_1/NewClassTesting.java
@@ -3,6 +3,7 @@ package TestingPackage_1;
 public class NewClassTesting {
 	public static void main(String[] args) {
 		System.out.println("new class creation for github");
+	
 	}
 
 }
--- Testing_For_GitHub/src/TestingPackage_2/GitConflictExp.java
+++ Testing_For_GitHub/src/TestingPackage_2/GitConflictExp.java
@@ -3,6 +3,7 @@ package TestingPackage_2;
 public class GitConflictExp {
 	public static void main(String[] args) {
 		System.out.println("this the testing for git conflict explantion");
+		System.out.println("this line is created by the admin");
 	}
 
 }
