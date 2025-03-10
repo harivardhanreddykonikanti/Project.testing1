7507d0df1d032cb161267c6907617fe8412f311d
harivardhanreddykonikanti <harivardhanreddyking@gmail.com>
2025-03-10 17:07:39 +0530

    in the file name GIT Conflict and line is created by the sam

--- Testing_For_GitHub/src/TestingPackage_2/GitConflictExp.java
+++ Testing_For_GitHub/src/TestingPackage_2/GitConflictExp.java
@@ -4,7 +4,7 @@ public class GitConflictExp {
 	public static void main(String[] args) {
 		System.out.println("this the testing for git conflict explantion");
 		System.out.println("this line is created by the admin");
-		System.out.println("this line is  created by the TE-1 SAM");
+		System.out.println("this line is  created by the TE-1 SAM Purpose only ");
 	}
 
 }
